3f70d6cdd853c0d1b8f1b337ef462ba9be49d28c
Amit Maurya <amitmaurya11042000@gmail.com>
2020-04-10 19:44:05 +0530

    Simple Dice Changing App using FlatButton.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue[900],
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
@@ -15,9 +17,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var RightDiceNumber = 1;
+
+  void changeDices(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      RightDiceNumber= Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          // Expandible is Smart Sizing of an image on phone screen
+          // Flex is the portion of the screen that you want it to acquire
+          Expanded(
+            flex: 1,
+            child:FlatButton(
+              // () ==> Empty because no argument given as an input
+              onPressed: (){
+                changeDices();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+
+          // Expandible is Smart Sizing of an image on phone screen
+          // Flex is the portion of the screen that you want it to acquire
+          Expanded(
+            child: FlatButton(
+              //() ==> Empty because no argument given as an input
+                onPressed: (){
+                  changeDices();
+                },
+                child: Image.asset('images/dice$RightDiceNumber.png')
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
+
